b5f5f254c2e22720b1d0d98f383e10a684dc60a2
felixNyalenda <felixasejunior@gmail.com>
2023-03-09 00:13:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// after index.js is processed
+// has a 'newHeader' variable that points to an <h1> node
+
+document.getElementById("main").remove();
+
+// after index.js is processed
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+const newHeader = document.createElement("h1");
+
+// after index.js is processed
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.id = "victory";
+
+// after index.js is processed
+// the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it:
+newHeader.innerHTML = "<h1>Felix is the champion</h1>";
